6f0b2e3032a2c18f1be871e5b07932a1bad5010b
menezesr08 <menezesr08@gmail.com>
2020-09-10 16:41:20 +0100

    Added filters and pagination

--- client/src/app/shared/models/pagination.ts
+++ client/src/app/shared/models/pagination.ts
@@ -1,9 +1,8 @@
 import { IProduct } from './product';
-
+// This interface returns the list of products and the pagination details from the api
 export interface IPagination {
     pageIndex: number;
     pageSize: number;
     count: number;
     data: IProduct[];
   }
-  
